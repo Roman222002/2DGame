efc8b425617f70e1078b23d7d211507a01a1c20d
stalnoj-ogur4ik <kilubaka@gmail.com>
2020-02-09 17:57:20 +0200

    + borders, main square - non lastic

--- Source.cpp
+++ Source.cpp
@@ -14,7 +14,8 @@ void updateMas_timer() {
 	}
 }
 
-int I = 5, J = 6;
+int I = 10, J = 10;
+char temp = ' ';
 
 void make_map(RenderWindow& win, sf::String TileMap[]) {
 
@@ -28,45 +29,45 @@ void make_map(RenderWindow& win, sf::String TileMap[]) {
 	s_map.setTexture(map);							//заливаЇм текстуру спрайтом
 
 	
+	
 
-	if ((Keyboard::isKeyPressed(Keyboard::Left) || (Keyboard::isKeyPressed(Keyboard::A)))) {
+	if ((Keyboard::isKeyPressed(Keyboard::Left) || (Keyboard::isKeyPressed(Keyboard::A)))&& J>1) {
+		TileMap[I][J] = temp;
+		temp = TileMap[I][J-1];
 		TileMap[I][J-1] = '5';
-		TileMap[I][J] = ' '; 
 		J--;
 	}
 
-	if ((Keyboard::isKeyPressed(Keyboard::Right) || (Keyboard::isKeyPressed(Keyboard::D)))) {
+	if ((Keyboard::isKeyPressed(Keyboard::Right) || (Keyboard::isKeyPressed(Keyboard::D)))&& J<WIDTH_MAP-2) {
+		TileMap[I][J] = temp;
+		temp = TileMap[I][J + 1];
 		TileMap[I][J+1] = '5';
-		TileMap[I][J] = ' '; 
 		J++;
 	}
 	
-	if (Keyboard::isKeyPressed(Keyboard::Up) || Keyboard::isKeyPressed(Keyboard::W)) {
+	if ((Keyboard::isKeyPressed(Keyboard::Up) || Keyboard::isKeyPressed(Keyboard::W))&& I>1) {
+		TileMap[I][J] = temp;
+		temp = TileMap[I - 1][J];
 		TileMap[I-1][J] = '5';
-		TileMap[I][J] = ' '; 
 		I--;
 	}
 
-	if (Keyboard::isKeyPressed(Keyboard::Down) || Keyboard::isKeyPressed(Keyboard::S)) {
-		TileMap[I+1][J] = '5';
-		TileMap[I][J] = ' '; 
+	if ((Keyboard::isKeyPressed(Keyboard::Down) || Keyboard::isKeyPressed(Keyboard::S))&& I<HEIGHT_MAP-2) {
+		TileMap[I][J] = temp;
+		temp = TileMap[I + 1][J];
+		TileMap[I + 1][J] = '5';
 		I++;
 	}
 
 	for (int i = 0; i < HEIGHT_MAP; i++)
 		for (int j = 0; j < WIDTH_MAP; j++)
 		{
-			
-
-
 			if (TileMap[i][j] == '0') s_map.setTextureRect(IntRect(224, 96, 256, 128));
-			else if (TileMap[i][j] == 's') s_map.setTextureRect(IntRect(32, 0, 64, 32));
+			else if (TileMap[i][j] == 's') s_map.setTextureRect(IntRect(224, 64, 256, 96));
 			else if (TileMap[i][j] == ' ') s_map.setTextureRect(IntRect(160, 64, 192, 96));
 			else if (TileMap[i][j] == '5') { 
 				s_map.setTextureRect(IntRect(64, 128, 96, 160)); 
 			}
-
-			
 
 			s_map.setPosition(j * 32, i * 32); //розбиваЇм на квадрати 32х32, робл€чи карту. 
 
